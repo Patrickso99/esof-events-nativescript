9da4e46a91bd583389485c20f3e0f7709fdb5fb5
Patrick Reichert <patrickreichert@MacBook-Pro-di-its.local>
2019-11-26 16:41:21 +0100

    data, style

---
+++ app/schedule/data.js
@@ -0,0 +1,24 @@
+module.exports =
+[
+        {
+          "nome": "Colazione",
+          "inizio": "8:00",
+          "fine": "9:00",
+          "luogo": "Caffè degli specchi",
+          "id": "162gs38dh"
+        },
+        {
+          "nome": "Presentazione ESOF 2019",
+          "inizio": "9:00",
+          "fine": "10:00",
+          "luogo": "Piazza Unità d'Italia",
+          "id": "@263gs79"
+        },
+        {
+          "nome": "Fine",
+          "inizio": "10:00",
+          "fine": "11:00",
+          "luogo": "Piazza Unità d'Italia",
+          "id": "@263gs79"
+        }
+]
