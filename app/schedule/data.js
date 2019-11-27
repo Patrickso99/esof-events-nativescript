7d791b153a81ba1ef1d42b375ab5c3ab9f47928e
Patrick Reichert <patrickreichert@MacBook-Pro-di-its.local>
2019-11-27 14:51:13 +0100

    Add plugin nativescript-ui-listview

--- app/schedule/data.js
+++ app/schedule/data.js
@@ -5,20 +5,23 @@ module.exports =
           "inizio": "8:00",
           "fine": "9:00",
           "luogo": "Caffè degli specchi",
-          "id": "162gs38dh"
+          "id": "162gs38dh",
+          "group": "8:00 - 9:00"
         },
         {
           "nome": "Presentazione ESOF 2019",
           "inizio": "9:00",
           "fine": "10:00",
           "luogo": "Piazza Unità d'Italia",
-          "id": "@263gs79"
+          "id": "@263gs79",
+          "group": "8:00 - 9:00"
         },
         {
           "nome": "Fine",
           "inizio": "10:00",
           "fine": "11:00",
           "luogo": "Piazza Unità d'Italia",
-          "id": "@263gs79"
+          "id": "@263gs79",
+          "group": "9:00 - 10:00"
         }
 ]
