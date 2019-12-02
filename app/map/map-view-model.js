f2e1770ab8cced11362f7b3e8a6bf5b5a2bff6fe
Patrick Reichert <patrickreichert@MacBook-Pro-di-its.local>
2019-12-02 17:14:28 +0100

    two data binding

--- app/map/map-view-model.js
+++ app/map/map-view-model.js
@@ -3,6 +3,8 @@ const observableModule = require("tns-core-modules/data/observable");
 function MapViewModel() {
     const viewModel = observableModule.fromObject({
         /* Add your view model properties here */
+        nome: '',
+        cognome: ''
     });
 
     return viewModel;
