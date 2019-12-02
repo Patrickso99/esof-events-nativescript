8d21d1e3915b45325bf168ec5e584633e40e8200
Patrick Reichert <patrickreichert@MacBook-Pro-di-its.local>
2019-12-02 17:03:48 +0100

    speakers by data

--- app/speakers/speakers-view-model.js
+++ app/speakers/speakers-view-model.js
@@ -1,11 +1,21 @@
 const observableModule = require("tns-core-modules/data/observable");
+const data = require('./data');
+const redata = data.map((item) => {
+    if(item.cat=='A')
+    {
+        item.color = 'red'
+    }
+
+    item.interventoCompleto = `${item.nome} - ${item.inizio} ${item.fine}`
+    return item
+});
 
 function SpeakersViewModel() {
     const viewModel = observableModule.fromObject({
-        /* Add your view model properties here */
-        test: 'Ciao mondo'
+        items: redata
     });
 
+    //console.log('FIRE: ', viewModel);
     return viewModel;
 }
 
