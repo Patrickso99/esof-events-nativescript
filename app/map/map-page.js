0116edb379c4085f219caad05a1b5d0d477a2c22
Patrick Reichert <patrickreichert@MBP-di-its.homenet.telecomitalia.it>
2019-12-07 11:12:31 +0100

    add markers form geojson data

--- app/map/map-page.js
+++ app/map/map-page.js
@@ -1,9 +1,30 @@
 const MapViewModel = require("./map-view-model");
 const mapsModule = require("nativescript-google-maps-sdk");
+const geoJson = require("./data");
 
-function onNavigatingTo(args) {
+function onNavigatingTo(args)
+{
     const component = args.object;
     component.bindingContext = new MapViewModel();
+}
+
+function isValidGEOJSON(obj)
+{
+    try
+    {
+        if (obj.hasOwnProperty('features') && obj.features.length > 0)
+        {
+            return true
+        }
+        else
+        {
+            console.error('Il tuo geoJson non è valido!')
+        }
+    }
+    catch
+    {
+      return false;
+    }
 }
 
 function onMapReady(args)
@@ -11,7 +32,30 @@ function onMapReady(args)
     //console.log("onMapReady", args);
     const mapView = args.object;
 
-    // Primo marker
+    // Se il geoJSON è valido, allora...
+    if (isValidGEOJSON(geoJson))
+    {
+        geoJson.features.forEach(feature => {
+
+            console.log('feature', feature.geometry.coordinates[0]);
+
+            const marker = new mapsModule.Marker();
+
+            marker.position = mapsModule.Position.positionFromLatLng (
+
+                feature.geometry.coordinates[1],
+                feature.geometry.coordinates[0]
+            );
+
+            marker.title = feature.properties.evento;
+            marker.color = "blue";
+            marker.userData = {index: 1};
+
+            mapView.addMarker(marker);
+        });
+    }
+
+    /* // Primo marker
     const primoMarker = new mapsModule.Marker();
 
     primoMarker.position = mapsModule.Position.positionFromLatLng (
@@ -33,7 +77,7 @@ function onMapReady(args)
     secondoMarker.title = "Pranzo";
     secondoMarker.color = "red";
     secondoMarker.userData = {index: 1};
-    mapView.addMarker(secondoMarker);
+    mapView.addMarker(secondoMarker); */
 }
 
 exports.onMapReady = onMapReady;
