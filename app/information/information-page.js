88a2ad801ea5e9712f55cc5089ee3bd4033456d0
Patrick Reichert <patrickreichert@MacBook-Pro-di-its.local>
2019-12-16 15:43:37 +0100

    add navigation function

--- app/information/information-page.js
+++ app/information/information-page.js
@@ -1,8 +1,18 @@
 const InformationViewModel = require("./information-view-model");
 
-function onNavigatingTo(args) {
+function onNavigatingTo(args)
+{
     const component = args.object;
     component.bindingContext = new InformationViewModel();
 }
 
+function onTap(args)
+{
+    const button = args.object;
+    const page = button.page;
+    page.frame.navigate("map/map-page");
+    // alert('FIRE!!!!');
+}
+
 exports.onNavigatingTo = onNavigatingTo;
+exports.onTap = onTap;
